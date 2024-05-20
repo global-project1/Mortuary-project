c6549ee2b085f8f31262159a4dbd023c34cbcc09
Joycenj <154092088+Joycenj@users.noreply.github.com>
2024-05-20 20:03:43 +0100

    Add files via upload

---
+++ request.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/request.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+                <center>
+            
+                <form  method="post"><br>
+                <h1>REQUEST</h1><br><br>
+                <input type="text" name="uname" placeholder="enter your name"><br><br>
+                <input type="email" name="email" placeholder="enter your email" ><br><br>
+                <input type="password" name="corpse_id" placeholder="enter corpse_id"><br><br>
+                <input type="date"><br><br>
+                <input type="submit" value="request"><br><br>
+                </form>
+                
+                </center> 
+    
+</body>
+</html>
